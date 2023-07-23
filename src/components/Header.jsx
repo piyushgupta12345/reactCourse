c035d27e3a4dda5debcf0424526b3f7dd2584230
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-07-23 21:39:04 +0530

    Components import export

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1>This is Header</h1>
+  )
+}
+
+function Header2() {
+  return (
+    <h1>This is Header2</h1>
+  )
+}
+
+function Header3() {
+    return (
+      <h1>This is Header2</h1>
+    )
+}
+
+export {Header2, Header3}
+export default Header
