64085e073317b11d0707c032f934672b1dc51106
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-07-23 21:51:21 +0530

    style css react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <h1>This is Header</h1>
+    <h1 className='header'>This is Header</h1>
   )
 }
 
 function Header2() {
+
+    const header2 = {
+        background: "black",
+        fontSize: 20,
+        color: "#fff"
+    }
   return (
-    <h1>This is Header2</h1>
+    <h1 className='header2' style={header2}>This is Header2</h1>
   )
 }
 
 function Header3() {
     return (
-      <h1>This is Header2</h1>
+      <h1 className='header3' style={{fontSize: 20, border: "2px solid red"}}>This is Header2</h1>
     )
 }
 
