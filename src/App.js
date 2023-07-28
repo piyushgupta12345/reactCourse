02a26f1262b4d42a1ba94115f17186076cc45313
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-07-28 14:33:58 +0530

    React-Router-dom

--- src/App.js
+++ src/App.js
@@ -1,48 +1,28 @@
 import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 import './App.css'
-import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
 
-  const [state, setState] = useState(2);
-  // const [state2, setState2] = useState(0);
-  const [data, setData] = useState([]);
-
-
-  useEffect(()=>{
-    async function getData() {
-      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
-
-      const res = await get.json()
-      setData(res)
-      // console.log(res)
-    }
-
-    getData();
-
-    document.title = `(${state} wow)`;
-
-  },[state])
-
-  // console.log("Function Body");
-
   return (
-    <div className="App">
-      <Header />
-      <button onClick={()=>setState(state + 1)}>Click Me {state}</button>
-      {/* <button onClick={()=>setState2(state2 + 1)}>Click Me {state2}</button> */}
-      {
-        data.map((element, index)=>{
-          return(
-            <div className="key" key={index}>
-              <h4>{element.firstName}</h4>
-              <h4>{element.lastName}</h4>
-              <h4>{element.email}</h4>
-            </div>
-          )
-        })
-      }
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          {/* About Routes */}
+          <Route path="/about" element={<About />}>
+          </Route>
+
+          {/* Contact Routes */}
+          <Route path="/contact" element={<Contact />}>
+          </Route>
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+
 
 function Header() {
   return (
     <div className="App">
-      <h1>useEffect Hook</h1>
+      <Link to={'/about'}><h1>About</h1></Link>
+      <Link to={'/contact'}><h1>Contact</h1></Link>
     </div>
   )
 }
